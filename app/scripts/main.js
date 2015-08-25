974d00edfdbefc71df96d5d0106dcc91030ced11
Aleksandr <alex.ru69@example.ru>
2015-08-25 11:26:42 +0300

    Change backbone script

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,61 +1,76 @@
-$(document).ready(function () {
-    st = new Statement();
-    fv = new FormView();
-    sv = new StatementView();
+var app = app || {};
+
+$(function () {
+    var statement = new app.Statement();
+    new app.FormView({model:statement});
+    new app.StatementView({model:statement});
 });
 
-var Statement = Backbone.Model.extend({
-    defaults: {
-        name: "",
-        faculty: "",
-        course: "",
-        statement: "",
-        date: ""
-    },
+(function () {
+    app.Statement = Backbone.Model.extend({
+        defaults: {
+            name: "",
+            faculty: "",
+            course: "",
+            statement: "",
+            date: ""
+        },
 
-    validate: function (attrs, options) {
-        console.log(attrs.name);
-    }
-});
+        validate: function (attrs, options) {
+            console.log(attrs);
+        }
+    });
+})();
 
-var StatementView = Backbone.View.extend({
-    el: '.document',
+(function ($) {
+    app.StatementView = Backbone.View.extend({
+        el: '.document',
 
-    initialize: function () {
-        this.$name = this.$("#out-name");
-        this.$faculty = this.$("#out-faculty");
-        this.$course = this.$("#out-course");
-        this.$statement = this.$("#out-statement");
-        this.$date = this.$("#out-date");
+        initialize: function () {
+            this.fields = {
+                'name' : this.$("#out-name"),
+                'faculty' : this.$("#out-faculty"),
+                'course' : this.$("#out-course"),
+                'statement' : this.$("#out-statement"),
+                'date' : this.$("#out-date")
+            };
+            this.listenTo(this.model, 'change', this.updateDocument);
+        },
 
-//        this.listenTo(st, 'change:name', this.render);
-        this.listenTo(st, 'change', this.render);
-    },
+        render: function () {
+            this.fields.name.html(this.model.get('name'));
+            this.fields.faculty.html(this.model.get('faculty'));
+            this.fields.course.html(this.model.get('course'));
+            this.fields.statement.html(this.model.get('statement'));
+            this.fields.date.html(this.model.get('date'));
+        },
 
-    render: function () {
-        this.$name.html(st.get('name'));
-        this.$faculty.html(st.get('faculty'));
-        this.$course.html(st.get('course'));
-        this.$statement.html(st.get('statement'));
-        this.$date.html(st.get('date'));
-    }
-});
+        updateDocument: function () {
+            var self = this;
+            _.each(this.model.changedAttributes(),function(value,key){
+                self.fields[key].html(value);
+            });
+        }
+    });
+})(jQuery);
 
-var FormView = Backbone.View.extend({
-    tagName: 'form',
+(function ($) {
+    app.FormView = Backbone.View.extend({
+        tagName: 'form',
 
-    el: 'form',
+        el: 'form',
 
-    events: {
-        "change textarea": "edit",
-        "change input": "edit"
-    },
+        events: {
+            "change textarea": "edit",
+            "change input": "edit"
+        },
 
-    edit: function (e) {
-        var value = e.target.value.trim();
-        var fieldModel = e.target.id.split('-')[1];
-        var obj = {};
-        obj[fieldModel] = value;
-        st.set(obj, {validate:true});
-    }
-});
+        edit: function (e) {
+            var value = e.target.value.trim();
+            var fieldModel = e.target.id.split('-')[1];
+            var obj = {};
+            obj[fieldModel] = value;
+            this.model.set(obj, {validate:true});
+        }
+    });
+})(jQuery);
